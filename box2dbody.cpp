a66d412a172cfb51fbea0b27fdc5dbf84e88e4a6
Gael Reinaudi <gael.reinaudi@gmail.com>
2014-04-27 00:23:47 -0400

    dup

--- box2dbody.cpp
+++ box2dbody.cpp
@@ -325,12 +325,6 @@ void Box2DBody::applyAngularImpulse(qreal impulse)
         mBody->ApplyAngularImpulse(impulse, true);
 }
 
-void Box2DBody::applyAngularImpulse(qreal impulse)
-{
-    if (mBody)
-        mBody->ApplyAngularImpulse(impulse, true);
-}
-
 void Box2DBody::applyTorque(qreal torque)
 {
     if (mBody)
